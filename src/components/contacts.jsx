a7618adacde0151c605284b0f8f354566c82a727
Molnia00 <bilankati@gmail.com>
2025-01-19 15:24:19 +0200

    update list

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -1,12 +1,12 @@
 import { HiUser } from 'react-icons/hi';
 import { BsFillTelephoneFill } from 'react-icons/bs';
-
+import s from './contact.module.css'
 
 function Contact({ name, number }) {
     return (
         <div>
 
-            <div>
+            <div className={s.contactContainer}>
                 <p><HiUser size="24" /> {name}</p>
             
                 <p><BsFillTelephoneFill size="24" /> {number}</p>
